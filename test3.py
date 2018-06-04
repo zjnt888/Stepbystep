01d16b23eca9a66e3cb4ca327431a7e479f805c8
zjnt888 <zjnt888@gmail.com>
2018-06-04 17:15:48 +0800

    打印去除了空白

--- test3.py
+++ test3.py
@@ -11,6 +11,7 @@ soup = BeautifulSoup(html, 'lxml')
 '''
 在(class_="read-top")中  文章名在<p></p>标签下，章节名在<span>标签下
 用beautifulsoup将其取出，分别打印
+
 '''
 title = soup.find(class_="read-top").find('h2')
 print('书名  ',title.a.get_text())
@@ -22,8 +23,9 @@ print('章节  ',title.span.get_text())
 docs = soup.find(class_="read-content", id="read-content").find_all('p')
 '''
 将文章段使用for语句一一取出打印，用if语句将最后含有打击盗版的一段不打印。
+“strip=True”参数去除前后空白
 '''
 for doc in docs:
     if '打击盗版' not in doc.get_text():
-        print(doc.get_text())
+        print(doc.get_text(strip=True))
         print('------------------')
